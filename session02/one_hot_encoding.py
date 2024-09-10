a5b50b8f5c01a5fa028d105fea7b8f9b369b2af9
jcorreia11 <jfscorreia95@gmail.com>
2024-09-10 11:14:01 +0100

    [add] one-hot-encoding exercise

---
+++ session02/one_hot_encoding.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+class OneHotEncoder:
+    """
+    One-hot encoder for categorical data.
+    """
+
+    def __init__(self):
+        """
+        One-hot encoder for categorical data.
+
+        Attributes
+        ----------
+        categories: list
+            List of unique categories in the data.
+        """
+        self.categories = None
+
+    def fit(self, data: list):
+        """
+        Fit the encoder by determining unique categories in the data.
+
+        Parameters
+        ----------
+        data: list
+            List of categorical data
+        """
+        # Find unique categories in the data and sort them
+        # add your code here
+        # ...
+
+    def transform(self, data: list) -> np.ndarray:
+        """
+        Transform the categorical data into one-hot encoded vectors.
+
+        Parameters
+        ----------
+        data: list
+            List of categorical data
+
+        Returns
+        -------
+        np.ndarray
+            Numpy array of one-hot encoded vectors
+        """
+        # Check if the encoder has been fitted
+        # add your code here
+
+        # Encode the data (i.e., convert each category into a one-hot encoded vector where the index of the 1 corresponds to the category)
+        one_hot_encoded = []
+        # add your code here
+        # ...
+        return np.array(one_hot_encoded)
+
+    def fit_transform(self, data: list) -> np.ndarray:
+        """
+        Fit and transform the data in a single step.
+
+        Parameters
+        ----------
+        data: list
+            List of categorical data
+
+        Returns
+        -------
+        np.ndarray
+            Numpy array of one-hot encoded vectors
+        """
+        self.fit(data)
+        return self.transform(data)
+
+    def inverse_transform(self, encoded_data: np.ndarray) -> list:
+        """
+        Convert one-hot encoded data back into categorical values.
+
+        Parameters
+        ----------
+        encoded_data: np.ndarray
+            Numpy array of one-hot encoded vectors
+
+        Returns
+        -------
+        list
+            List of categorical data.
+        """
+        if self.categories is None:
+            raise Exception("The encoder has not been fitted yet.")
+
+        inverse_data = []
+        # Convert one-hot encoded data back into categorical values
+        # add your code here
+        # ...
+        return inverse_data
+
+if __name__ == '__main__':
+    encoder = OneHotEncoder()
+    data = ['cat', 'dog', 'cat', 'fish', 'dog']
+    # What output do you expect here?
+    encoder.fit(data)
+    print(encoder.categories)
+    print(encoder.transform(data))
+    print(encoder.fit_transform(data))
+
+    # What output do you expect here?
+    encoded_data = np.array([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 0, 1], [0, 1, 0]])
+    print(encoder.inverse_transform(encoded_data))
