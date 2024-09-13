9e7f7f2337ea9767bffa52f7a7c7b98acf96103e
João Correia <jfscorreia95@gmail.com>
2024-09-13 15:29:13 +0100

    [ADD] stopword exercise

---
+++ exercises/session02/stopword_removal.py
@@ -0,0 +1,27 @@
+from typing import List, Set
+
+
+def remove_stopwords(tokens: List[str], stopwords: Set[str]) -> List[str]:
+    """
+    Remove stopwords from tokens.
+
+    Parameters
+    ----------
+    tokens: List[str]
+        List of tokens.
+    stopwords: Set[str]
+        Set of stopwords.
+
+    Returns
+    -------
+    List[str]
+        List of tokens without stopwords.
+    """
+    # add code here
+    # ...
+
+if __name__ == '__main__':
+    stopwords = {'a', 'is', 'it', 'this', 'for'}
+    tokens = ['this', 'is', 'a', 'sample', 'text', 'for', 'token', 'removal']
+    print(remove_stopwords(tokens, stopwords))
+    assert remove_stopwords(tokens, stopwords) == ['sample', 'text', 'token', 'removal']
