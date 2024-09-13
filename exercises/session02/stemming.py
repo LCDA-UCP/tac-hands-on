ced49cb8ab0fd6bcd1c2b6fe3efeddc17c407e9b
João Correia <jfscorreia95@gmail.com>
2024-09-13 16:12:46 +0100

    [ADD] stemming exercise

---
+++ exercises/session02/stemming.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def suffix_s_stemmer(terms: List[str]) -> List[str]:
+    """
+    Removes the s-suffix from all terms in a sequence.
+
+    Parameters
+    ----------
+    terms : list of str
+        A list of terms.
+
+    Returns
+    -------
+    list of str
+        A list of terms without the s-suffix.
+    """
+    stemmed_terms = []
+    # add code here
+    # ...
+    return stemmed_terms
+
+if __name__ == '__main__':
+    terms = ["dogs", "cats", "houses", "mice"]
+    print(suffix_s_stemmer(terms))
+    assert suffix_s_stemmer(terms) == ["dog", "cat", "house", "mice"]
