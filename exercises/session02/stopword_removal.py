b21a46d1a78cf28bf7efb7c8217518da7232465d
jcorreia11 <jfscorreia95@gmail.com>
2024-09-20 10:45:23 +0100

    Solve exercise stopword removal

--- exercises/session02/stopword_removal.py
+++ exercises/session02/stopword_removal.py
@@ -1,5 +1,25 @@
 from typing import List, Set
 
+from pandas.core.computation.parsing import tokenize_string
+def remove_stopwords2(tokens: List[str], stopwords: Set[str]) -> List[str]:
+    """
+    Remove stopwords from tokens.
+
+    Parameters
+    ----------
+    tokens: List[str]
+        List of tokens.
+    stopwords: Set[str]
+        Set of stopwords.
+
+    Returns
+    -------
+    List[str]
+        List of tokens without stopwords.
+    """
+    # add code here
+    return [token for token in tokens if token not in stopwords]
+
 
 def remove_stopwords(tokens: List[str], stopwords: Set[str]) -> List[str]:
     """
@@ -18,7 +38,11 @@ def remove_stopwords(tokens: List[str], stopwords: Set[str]) -> List[str]:
         List of tokens without stopwords.
     """
     # add code here
-    # ...
+    for token in tokens:
+        if token in stopwords:
+            tokens.remove(token)
+    return tokens
+
 
 if __name__ == '__main__':
     stopwords = {'a', 'is', 'it', 'this', 'for'}
