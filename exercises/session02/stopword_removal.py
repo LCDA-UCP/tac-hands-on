9c133147b9ba4735d656786b0b977ac92db5d899
beatrizdias <diasbeatriz40@gmail.com>
2024-09-20 10:45:55 +0100

    solve exercise stopword removal

--- exercises/session02/stopword_removal.py
+++ exercises/session02/stopword_removal.py
@@ -18,7 +18,10 @@ def remove_stopwords(tokens: List[str], stopwords: Set[str]) -> List[str]:
         List of tokens without stopwords.
     """
     # add code here
-    # ...
+    for token in tokens:
+        if token in stopwords:
+            tokens.remove(token)
+    return tokens
 
 if __name__ == '__main__':
     stopwords = {'a', 'is', 'it', 'this', 'for'}
