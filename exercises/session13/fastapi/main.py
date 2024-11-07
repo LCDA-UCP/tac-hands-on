5bbd350d77fbf745345b8cb87f90a6efe145f4d2
tiago <tiagosilvavieira06@gmail.com>
2024-11-07 14:51:49 +0000

    Solve exercice

---
+++ exercises/session13/fastapi/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+todo_list = []
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+@app.post("/items")
+def create_item(item: str):
+    todo_list.append(item)
+    return {"message": f"Item {item} added successfully\n"}
+
+@app.get("/items")
+def get_items():
+    return {"todo_list": todo_list}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    if item_id < len(todo_list):
+        return {"item": todo_list[item_id]}
+    else:
+        return {"message": "Item not found"}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    if item_id < len(todo_list):
+        del todo_list[item_id]
+        return {"message": "Item"}
